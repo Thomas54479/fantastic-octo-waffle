17458521197a7d8f2096e52a22520c202ac63d9d
Thomas54479 <phirithomas952@gmail.com>
2026-02-06 22:13:16 +0200

    Create validation middleware for request validation using Joi.

---
+++ src/middleware/validation.js
@@ -0,0 +1,18 @@
+const Joi = require('joi');
+
+// Example validation schema
+const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+});
+
+const validateRequest = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+    next();
+};
+
+module.exports = validateRequest;
