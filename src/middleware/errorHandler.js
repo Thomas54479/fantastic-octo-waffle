48dd00a47bc482a6756dd778dcb7589d46181bb9
Thomas54479 <phirithomas952@gmail.com>
2026-02-06 22:12:42 +0200

    Create error handling middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Error handling middleware.
+ * @param {Error} err - The error object.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        message: 'Something went wrong!',
+        error: err.message
+    });
+};
+
+module.exports = errorHandler;
